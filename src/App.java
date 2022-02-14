9acfe40d0f9081550135c820786086b28dc849e4
Drakel <andrewemerick16@protonmail.com>
2022-02-13 19:01:54 -0500

    initialize git repo for ascii encrypt program in java

---
+++ src/App.java
@@ -0,0 +1,32 @@
+/*
+    Realizado por Andrew Emerick
+    12/2/22
+    Cambia frases a ASCII igual y vice versa
+*/
+
+import java.util.Scanner;
+
+public class App {
+
+    static void encrypt(String sentence){
+        for(byte charVal=0; charVal<sentence.length(); charVal++) {
+            System.out.print((byte)sentence.charAt(charVal)+"/");
+        }
+        System.out.print("hello");
+    }
+
+    static void decrypt(String sentence){
+        /*String[] byteString = sentence.split("/");
+        byte[] ascii = new byte[];
+        System.out.print(ascii[1]);*/
+    }
+      public static void main(String[] args) throws Exception {
+        try (Scanner vampire = new Scanner(System.in)) {
+
+            System.out.println("Enter a sentence: ");
+            String sentence = vampire.nextLine();
+
+            decrypt(sentence);
+        }
+    }
+}
