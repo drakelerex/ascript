622e7c9f4d799fb229388f6880a8a3b774a6dcd0
Andrew Emerick <andrewemerick16@protonmail.com>
2022-02-24 14:46:45 -0500

    completed code in methods

--- src/ascript/ascript.java
+++ src/ascript/ascript.java
@@ -8,6 +8,7 @@ package ascript;
 
 import java.nio.charset.StandardCharsets;
 import java.util.Scanner;
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
@@ -38,13 +39,40 @@ public class ascript {
             long i=0;
             String s="";
             while((i=inFile.read())!=-1){    
-                System.out.print((char)i);    
                 s += i+"/";
             }
-            System.out.println(s);
+            System.out.println("File Written to output.txt");
             inFile.close();
             FileOutputStream fileOut = new FileOutputStream("output.txt");
             byte[] sArray = s.getBytes(StandardCharsets.UTF_8);
+            fileOut.write(sArray);
+            fileOut.close();
+        } catch(IOException e){
+            System.out.println("An error occured");
+            e.printStackTrace();
+        }
+    }
+
+    static void dFile(String fileName){
+        try{
+            FileInputStream inFile = new FileInputStream(fileName);
+            long ix=0;
+            String s="";
+            while((ix=inFile.read())!=-1){
+                s += (char)ix;
+            }
+            String[] byteString = s.split("/");
+            int size = byteString.length;
+            byte[] asciiBytes = new byte[size];
+            for(int i=0;i<size;i++){
+                asciiBytes[i] = Byte.parseByte(byteString[i]);
+            }
+            String fContent="";
+            for(int i=0;i<asciiBytes.length;i++){
+                fContent += (char)asciiBytes[i];
+            }
+            byte[] sArray = fContent.getBytes(StandardCharsets.UTF_8);
+            FileOutputStream fileOut = new FileOutputStream("output.txt");
             fileOut.write(sArray);
             fileOut.close();
         } catch(IOException e){
@@ -56,7 +84,7 @@ public class ascript {
         Scanner vampire = new Scanner(System.in);
         System.out.println("Enter a file name: ");
         String fileName = vampire.nextLine();
-        eFile(fileName);
+        dFile(fileName);
         vampire.close();
     }
 }
