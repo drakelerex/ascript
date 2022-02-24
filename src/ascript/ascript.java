267a68d6f18aca1434793e297062cb869e1bb9c9
Andrew Emerick <andrewemerick16@protonmail.com>
2022-02-24 15:21:38 -0500

    remove redundant code

--- src/ascript/ascript.java
+++ src/ascript/ascript.java
@@ -14,32 +14,13 @@ import java.io.IOException;
 
 public class ascript {
 
-    static void encrypt(String sentence){
-        byte[] asciiByte=sentence.getBytes(StandardCharsets.UTF_8);
-        for(byte i=0;i<asciiByte.length;i++)
-            System.out.print(asciiByte[i]+"/");
-    }
-
-    static void decrypt(String sentence){
-        String[] byteString = sentence.split("/");
-        int size = byteString.length;
-        byte[] asciiBytes = new byte[size];
-        for(int i=0;i<size;i++){
-            asciiBytes[i] = Byte.parseByte(byteString[i]);
-        }
-        for(byte i=0;i<asciiBytes.length;i++){
-            System.out.print((char)asciiBytes[i]);
-        }
-    }
-
     static void eFile(String fileName){
         try{
             FileInputStream inFile = new FileInputStream(fileName);
             long i=0;
             String s="";
-            while((i=inFile.read())!=-1){    
+            while((i=inFile.read())!=-1)   
                 s += i+"/";
-            }
             System.out.println("File Written to output.txt");
             inFile.close();
             FileOutputStream fileOut = new FileOutputStream("output.txt");
@@ -55,25 +36,18 @@ public class ascript {
     static void dFile(String fileName){
         try{
             FileInputStream inFile = new FileInputStream(fileName);
-            long ix=0;
+            int i=0;
             String s="";
-            while((ix=inFile.read())!=-1){
-                s += (char)ix;
-            }
+            while((i=inFile.read())!=-1)
+                s += (char)i;
             inFile.close();
             String[] byteString = s.split("/");
             int size = byteString.length;
             byte[] asciiBytes = new byte[size];
-            for(int i=0;i<size;i++){
+            for(i=0;i<size;i++)
                 asciiBytes[i] = Byte.parseByte(byteString[i]);
-            }
-            String fContent="";
-            for(int i=0;i<asciiBytes.length;i++){
-                fContent += (char)asciiBytes[i];
-            }
-            byte[] sArray = fContent.getBytes(StandardCharsets.UTF_8);
             FileOutputStream fileOut = new FileOutputStream("output.txt");
-            fileOut.write(sArray);
+            fileOut.write(asciiBytes);
             fileOut.close();
         } catch(IOException e){
             System.out.println("An error occured");
